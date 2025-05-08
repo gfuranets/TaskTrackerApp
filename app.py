f6114ac0e7c96dca2769cd21a6457a34fd004ad0
smokingLasagna <furanetsgeorgiy@gmail.com>
2025-05-08 12:15:21 +0200

    final commit before presenting 08.05.25

--- app.py
+++ app.py
@@ -12,19 +12,29 @@ from flask_bcrypt import Bcrypt
 
 from flask_migrate import Migrate
 
-from datetime import datetime
+from datetime import datetime, date
+import io
+import base64
+import matplotlib.pyplot as plt
+from functools import wraps
+from flask import make_response
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv() 
+print("SECRET_KEY =", os.getenv('SECRET_KEY'))
+
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-app.config['SECRET_KEY'] = '123'
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 db = SQLAlchemy(app)
 
-bcrypt = Bcrypt(app) # necessary for password hashing
+bcrypt = Bcrypt(app)
+migrate = Migrate(app, db)
 
-migrate = Migrate(app, db) # necessary for updating DB
-
-""" Ensure an account control system """
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
@@ -33,52 +43,73 @@ login_manager.login_view = 'login'
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-""" Define a user structure in the DB """
-""" Use UserMixin to fill all the required fields for the User class """
+""" Prevent repeated authentification error """
+def nocache(view):
+    @wraps(view)
+    def no_cache_view(*args, **kwargs):
+        response = make_response(view(*args, **kwargs))
+        response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0'
+        response.headers['Pragma'] = 'no-cache'
+        response.headers['Expires'] = '-1'
+        return response
+    return no_cache_view
+
+""" Upgrade task progress daily """
+def check_and_update_day(user):
+    today = str(date.today())
+
+    if user.stats is None:
+        user.stats = MutableDict()
+
+    if today not in user.stats:
+        user.stats[today] = {}
+
+    if user.tasks:
+        for task, (progress, total) in user.tasks.items():
+            percent = round((progress / total) * 100) if total > 0 else 0
+            user.stats[today][task] = percent
+
+    user.last_updated = today
+    flag_modified(user, 'stats')
+    flag_modified(user, 'last_updated')
+    db.session.commit()
+
+
 class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key = True)
-    username = db.Column(db.String(20), nullable = False, unique = True)
-    password = db.Column(db.String(80), nullable = False)
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False, unique=True)
+    password = db.Column(db.String(80), nullable=False)
     tasks = db.Column(MutableDict.as_mutable(PickleType), nullable=True)
+    stats = db.Column(MutableDict.as_mutable(PickleType), nullable=True)
+    last_updated = db.Column(db.String(10), nullable=True)
 
 
 class RegisterForm(FlaskForm):
-    username = StringField(validators = [InputRequired(), 
-                                         Length(min = 4, max = 20)],
-                                         render_kw = {'placeholder': 'Username'})
-    
-    password = PasswordField(validators = [InputRequired(),
-                                          Length(min = 4, max = 20)],
-                                          render_kw = {'placeholder': 'Password'})
-    
+    username = StringField(validators=[InputRequired(), Length(min=4, max=20)],
+                           render_kw={'placeholder': 'Username'})
+    password = PasswordField(validators=[InputRequired(), Length(min=4, max=20)],
+                             render_kw={'placeholder': 'Password'})
     submit = SubmitField('Register')
 
-    """ Prevent user from creating accounts with identical usernames """
     def validate_username(self, username):
-        existing_user_username = User.query.filter_by(
-            username = username.data).first()
+        existing_user_username = User.query.filter_by(username=username.data).first()
         if existing_user_username:
-            raise ValidationError(
-                'That username is already taken')
+            raise ValidationError('That username is already taken')
+
 
 class LoginForm(FlaskForm):
-    username = StringField(validators = [InputRequired(), 
-                                         Length(min = 4, max = 20)],
-                                         render_kw = {'placeholder': 'Username'})
-    
-    password = PasswordField(validators = [InputRequired(),
-                                          Length(min = 4, max = 100)], # max is smaller than the DB value due to hashing (max 20 original, 80 encrypted)
-                                          render_kw = {'placeholder': 'Password'})
-    
+    username = StringField(validators=[InputRequired(), Length(min=4, max=20)],
+                           render_kw={'placeholder': 'Username'})
+    password = PasswordField(validators=[InputRequired(), Length(min=4, max=100)],
+                             render_kw={'placeholder': 'Password'})
     submit = SubmitField('Login')
 
+
 class CreateForm(FlaskForm):
-    task = StringField(validators = [Length(min = 1, max = 60)],
-                                    render_kw = {'placeholder': 'Task'})
-    
-    amount = IntegerField(validators = [NumberRange(min = 1, max = 1e5)],
-                                        render_kw = {'placeholder': 'Amount'})
-    
+    task = StringField(validators=[Length(min=1, max=60)],
+                       render_kw={'placeholder': 'Task'})
+    amount = IntegerField(validators=[NumberRange(min=1, max=1e5)],
+                          render_kw={'placeholder': 'Amount'})
     submit = SubmitField('Create')
 
 
@@ -88,110 +119,104 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/signup', methods = ['GET', 'POST'])
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
     form = RegisterForm()
 
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        new_user = User(username = form.username.data, password = hashed_password)
+        new_user = User(username=form.username.data, password=hashed_password)
 
         db.session.add(new_user)
         db.session.commit()
 
         return redirect('/')
 
-    return render_template('signup.html', form = form)
+    return render_template('signup.html', form=form)
 
 
-@app.route('/login', methods = ['GET', 'POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
 
-    # First the program checks for a username in the DB, then hashes the input password and checks with the DB password
-    # If passwords match the user is redirected to logged in homepage
     if form.validate_on_submit():
-        user = User.query.filter_by(username = form.username.data).first() # Find a user with such username
-        if user:
-            if bcrypt.check_password_hash(user.password, form.password.data): # compare input password and DB password
-                login_user(user) # user has enter his account
-                return redirect('/indexLogged')
+        user = User.query.filter_by(username=form.username.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user)
+            return redirect('/indexLogged')
 
-    return render_template('login.html', form = form)
+    return render_template('login.html', form=form)
 
-@app.route('/logout', methods = ['GET', 'POST'])
+
+@app.route('/logout', methods=['GET', 'POST'])
 @login_required
+@nocache
 def logout():
     logout_user()
     return redirect('/index')
 
-@app.route('/indexLogged')
+
+@app.route('/indexLogged', methods=['GET', 'POST'])
 @login_required
+@nocache
 def indexLogged():
-    """ Set task progress to zero each day """
-    todays_date = str(datetime.today().date())[5:]
-    if todays_date != start_date:
-        for task in current_user.tasks:
-            current_user.tasks[task][0] = 0
-
-            flag_modified(current_user, 'tasks')
-            db.session.commit()
-
     tasks = current_user.tasks if current_user.tasks else {}
-    return render_template('indexLogged.html', tasks = tasks)
+    return render_template('indexLogged.html', tasks=tasks)
 
-@app.route('/create', methods = ['GET', 'POST'])
+
+@app.route('/create', methods=['POST', 'GET'])
 @login_required
+@nocache
 def create():
     form = CreateForm()
 
     if form.validate_on_submit():
-        # Acquire data from form
         task = form.task.data
         amount = form.amount.data
 
-        # Check whether there is a dict
         if current_user.tasks is None:
-            current_user.tasks = MutableDict() # needed for further changing
+            current_user.tasks = MutableDict()
 
-        # Add new task to the tasks dictionary
         current_user.tasks.update({task: [0, amount]})
         db.session.commit()
 
         return redirect('/indexLogged')
 
-    return render_template('create.html', form = form)
+    return render_template('create.html', form=form)
 
-@app.route('/delete', methods = ['POST'])
+
+@app.route('/delete', methods=['POST'])
 @login_required
+@nocache
 def delete():
     task = request.form.get('task')
 
-    if current_user.tasks != {}:
-        if task in current_user.tasks:
-            current_user.tasks.pop(task)
-
-            flag_modified(current_user, 'tasks')
-            db.session.commit()
+    if current_user.tasks and task in current_user.tasks:
+        current_user.tasks.pop(task)
+        flag_modified(current_user, 'tasks')
+        db.session.commit()
 
     return redirect('/indexLogged')
 
-@app.route('/increase', methods = ['POST'])
+
+@app.route('/increase', methods=['POST'])
 @login_required
+@nocache
 def increase():
     task = request.form.get('task')
     added_progress = int(request.form.get('added_progress'))
 
     if added_progress > 0:
         current_user.tasks[task][0] += added_progress
-
         flag_modified(current_user, 'tasks')
         db.session.commit()
 
     return redirect('/indexLogged')
 
-@app.route('/decrease', methods = ['POST'])
+
+@app.route('/decrease', methods=['POST'])
 @login_required
+@nocache
 def decrease():
     task = request.form.get('task')
     current_progress = int(request.form.get('current_progress'))
@@ -200,8 +225,7 @@ def decrease():
     if removed_progress > 0:
         if removed_progress <= current_progress:
             current_user.tasks[task][0] -= removed_progress
-
-        elif removed_progress > current_progress:
+        else:
             current_user.tasks[task][0] = 0
 
         flag_modified(current_user, 'tasks')
@@ -209,13 +233,44 @@ def decrease():
 
     return redirect('/indexLogged')
 
-@app.route('/statistics.html')
+
+@app.route('/statistics')
 @login_required
+@nocache
 def statistics():
-    return render_template('statistics.html')
+    check_and_update_day(current_user)
+    stats = current_user.stats or {}
+
+    task_progress = {}
+    for day, day_stats in stats.items():
+        for task, value in day_stats.items():
+            if task not in task_progress:
+                task_progress[task] = []
+            task_progress[task].append((day, value))
+
+    plt.figure(figsize=(10, 6))
+
+    for task, data in task_progress.items():
+        data.sort(key=lambda x: x[0])
+        dates = [str(d) for d, _ in data]
+        values = [float(v) for _, v in data]
+        plt.plot(dates, values, marker='o', label=task)
+
+    plt.xlabel('Date')
+    plt.ylabel('Progress (%)')
+    plt.title('Percentual task progresses')
+    plt.xticks(rotation=45)
+    plt.legend()
+    plt.tight_layout()
+
+    buf = io.BytesIO()
+    plt.savefig(buf, format='png')
+    buf.seek(0)
+    image_base64 = base64.b64encode(buf.read()).decode('utf-8')
+    buf.close()
+    plt.close()
+
+    return render_template('statistics.html', plot_url=image_base64, tasks=current_user.tasks)
 
 if __name__ == '__main__':
-    global start_date
-    start_date = str(datetime.today().date())[5:]
-
-    app.run(debug = True)
+    app.run(debug=True)
